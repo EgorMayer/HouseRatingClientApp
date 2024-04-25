b647ae56bb56d2a29493934c85a786d13f01b459
EgorMayer <egorkasavava@gmail.com>
2024-04-25 20:13:05 +0200

    Первичная работа с интерфейсом

---
+++ HouseRatingClientApp/ViewModel/Base/VIewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRatingClientApp.ViewModel.Base
+{
+    internal class VIewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ HouseRatingClientApp/ViewModel/LoginUserViewModel.cs
@@ -0,0 +1,46 @@
+﻿using HouseRatingClientApp.Infrastructure.Commands.Base;
+using HouseRatingClientApp.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HouseRatingClientApp.ViewModel
+{
+    internal class LoginUserViewModel : VIewModelBase
+    {
+        public ICommand CloseWindowCommand { get; }
+
+
+        public LoginUserViewModel()
+        {
+            CloseWindowCommand = new LambdaCommand(CloseApp);
+        }
+
+        public void CloseApp(object o)
+        {
+            Application.Current.Shutdown();
+        }
+
+        public void EnterIn()
+        {
+            //MainWindow mainWindow = new MainWindow(); //main Window.xaml
+            //EnterAppWindowViewModel viewModel = new EnterAppWindowViewModel();
+
+            //string json = JsonSerializer.Serialize("data.json");
+            //File.WriteAllText("data.json", json);
+
+            //var data = new { Login = , Password =  };
+            //string jsonString = JsonSerializer.Serialize(data);
+            //File.WriteAllText("data.json", jsonString);
+
+            //MessageBox.Show("Вы успешно вошли в аккаунт!");   
+
+            //CloseApp(viewModel);
+            //mainWindow.Show();
+        }
+    }
+}
